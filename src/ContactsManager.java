e3a18802533929b04f188ec3f7f057cb6ceeeb37
Soul-to-dev <0749sk@gmail.com>
2026-04-28 17:14:54 +0000

    Update ContactsManager.java

--- src/ContactsManager.java
+++ src/ContactsManager.java
@@ -1,24 +1,17 @@
-// Cette classe gère une liste de contacts
 public class ContactsManager {
 
-    // On crée un tableau qui peut contenir jusqu'à 10 contacts
     Contact[] listeContacts = new Contact[10];
 
-    // On compte combien de contacts ont été ajoutés
     int nombreDeContacts = 0;
 
-    // Méthode pour ajouter un contact dans la liste
     public void addContact(Contact contact) {
         listeContacts[nombreDeContacts] = contact;
         nombreDeContacts = nombreDeContacts + 1;
         System.out.println("Contact ajouté : " + contact.nom);
     }
 
-    // Méthode pour chercher un contact par son nom
     public void searchContact(String nom) {
-        // On parcourt tous les contacts un par un
         for (int i = 0; i < nombreDeContacts; i++) {
-            // Si on trouve le nom recherché
             if (listeContacts[i].nom.equals(nom)) {
                 System.out.println("Contact trouvé !");
                 System.out.println("Nom : " + listeContacts[i].nom);
@@ -26,7 +19,6 @@ public class ContactsManager {
                 return; // On arrête la recherche
             }
         }
-        // Si on arrive ici, le contact n'existe pas
         System.out.println("Contact introuvable : " + nom);
     }
 }
